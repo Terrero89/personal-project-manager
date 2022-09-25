49aff87dd0bbc0d6fa227bf8b56994e60e917f92
Sergio Terrero <60996043+Terrero89@users.noreply.github.com>
2022-09-25 13:47:25 -0400

    final fix on merge conflicts

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slot">
+    <UINavbar />
+
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.slot {
+  min-height: 100vh;
+  background-color: rgb(248, 246, 246);
+}
+</style>
