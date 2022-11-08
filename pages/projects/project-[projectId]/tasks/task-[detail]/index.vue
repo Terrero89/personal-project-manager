233312750b18d52b26b25444d2474c9f081d7a48
Terrero <sergio-terrero@hotmail.com>
2022-11-07 23:28:21 -0500

    reviwing

--- pages/projects/project-[projectId]/tasks/task-[detail]/index.vue
+++ pages/projects/project-[projectId]/tasks/task-[detail]/index.vue
@@ -9,7 +9,9 @@ const param = parseInt(route.params.projectId);
 const taskParam = parseInt(route.params.detail);
 const { tasks, projects, history, taskList, findLength, findActionsByTask } =
   store;
-
+const updateLink = computed(
+  () => `/projects/project-${param}/tasks/task-${taskParam}/u/update`
+);
 const projectParent = computed(() => store.filterItemById);
 const findTaskDetail = computed(() => store.detailOfTask); //finds the task place for details
 const length = store.hasTasks;
@@ -36,6 +38,7 @@ function deleteTask(id, parent) {
         v-for="project in findTaskDetail(taskParam)"
         :key="project.id"
       >
+        {{ taskParam }}
         <div class="container detail-container">
           <UITitle title="Task Details" class="border-bottom" />
 
@@ -83,19 +86,45 @@ function deleteTask(id, parent) {
               <p v-if="project.isComplete">Complete</p>
               <p v-if="!project.isComplete">In Progress</p>
               <div class="">
-              <button
-                type="button"
-                @click="deleteTask(taskParam, project.parentId)"
-                class="btn btn-danger"
-              >
-               Delete
-              </button>
-              <button type="button" class="btn btn-outline-primary">
-                Update
-              </button>
+                <button
+                  @click="removeItem(props.id)"
+                  type="button"
+                  class="btn btn-danger mr-5"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-trash-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+                    />
+                  </svg>
+                </button>
+                <Nuxt-Link :to="updateLink">
+                  <button type="button" class="btn btn-outline-primary">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-file-earmark-code"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"
+                      />
+                      <path
+                        d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708z"
+                      />
+                    </svg>
+                  </button>
+                </Nuxt-Link>
+              </div>
             </div>
-            </div>
-         
           </div>
         </div>
         <UICard>
@@ -133,5 +162,4 @@ function deleteTask(id, parent) {
   color: rgb(88, 88, 88);
   width: 90%;
 }
-
 </style>
