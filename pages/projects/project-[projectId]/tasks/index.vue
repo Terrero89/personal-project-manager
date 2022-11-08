62c7f5e0c6cae871de0cbe94fb56d2fba62db706
Sergio Terrero <60996043+Terrero89@users.noreply.github.com>
2022-11-08 18:17:07 -0500

    routes fixed and working

--- pages/projects/project-[projectId]/tasks/index.vue
+++ pages/projects/project-[projectId]/tasks/index.vue
@@ -1,135 +1,132 @@
-<!-- details task page -->
 <script setup>
 import { useTest } from "@/store/test";
 
 const store = useTest();
 const route = useRoute(); //route object
-const router = useRouter(); //route object
 const param = parseInt(route.params.projectId);
-const taskParam = parseInt(route.params.detail);
-const { tasks, projects, history, taskList, findLength, findActionsByTask } =
-  store;
 
-const projectParent = computed(() => store.filterItemById);
-const findTaskDetail = computed(() => store.detailOfTask); //finds the task place for details
+const { taskList, projectList } = store;
+const tasksOfParents = taskList.filter((task) => task.parentId == param); //needs fix
+const getParent = projectList.filter((p) => p.id == param); //needs fix
+const seeDetail = (parameter) => {
+  return parameter;
+}; //will make the id selectec the currect id to navigate
 const length = store.hasTasks;
-//function that deletes the item and return to projects page.
-function deleteTask(id, parent) {
-  store.deleteTask(id); //executes the delete action in pinia
-  store.deletedToActions(id, parent); //will redirect to tasks, or projects tasks depending on tasks length of the tasks
-  if (length(param) - 1 < 1) {
-    return navigateTo(`/projects/project-${param}`);
-  } else {
-    //will push to history those that match
-    history.push(tasks.find((t) => t.id === id)); //needs to e fixed
-    //after action is pushed to actions, navigate to project's tasks page
-    return navigateTo(`/projects/project-${param}/tasks`);
-  }
-}
 </script>
 
 <template>
-  <div class="tasks-wrapper">
+  <div class="tasks-wrapper table-responsive">
     <div class="task-list">
-      <div
-        class="project-detail"
-        v-for="project in findTaskDetail(taskParam)"
-        :key="project.id"
+      <UITitle title="Tasks" class="border-bottom" />
+      <div>add task feature</div>
+      <div>search bar</div>
+      <div>filtering</div>
+      back to parent-
+
+      <nuxt-link to="/projects">projects</nuxt-link>
+
+      <h3
+        style="color: black; font-size: size 1.5rem"
+        v-for="parent in getParent"
+        :key="parent.id"
       >
-        <div class="container detail-container">
-          <UITitle title="Task Details" class="border-bottom" />
+        {{ parent.projectName }}
+      </h3>
 
-          <div class="row">
-            <div class="header">
-              <h3
-                style="color: black; font-size: size 1.5rem"
-                v-for="task in findTaskDetail(taskParam)"
-                :key="task.id"
+      <table class="table">
+        <thead>
+          <tr class="table-header">
+            <th>ID</th>
+
+            <th>TASK NAME</th>
+            <th>STATUS</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            class="table-content"
+            v-for="task in tasksOfParents"
+            :key="task.id"
+          >
+            <td>{{ task.id }}</td>
+
+            <td>
+              {{ task.taskName }}
+            </td>
+
+            <td v-if="task.isComplete">Complete</td>
+            <td v-if="!task.isComplete">In Progress</td>
+            <td>
+              <nuxt-link
+                :to="`/projects/project-${param}/tasks/task-${seeDetail(
+                  task.id
+                )}`"
+                >Details</nuxt-link
               >
-                {{ task.taskName }}
-              </h3>
-            </div>
-
-            <div class="col">
-              <div class="detail">
-                <div class="content">
-                  <div class="item">Task ID</div>
-                  <p class="item-desc">{{ project.id }}</p>
-
-                  <div class="item">Parent Name</div>
-                  <p
-                    class="item-desc"
-                    v-for="parent in projectParent(param)"
-                    :key="parent.id"
-                  >
-                    {{ parent.projectName }}
-                  </p>
-
-                  <div class="item">Description</div>
-                  <p class="item-desc">{{ project.description }}</p>
-                  <div class="item">Start Date</div>
-                  <p class="item-desc">{{ project.startDate }}</p>
-                  <div class="item">End Date</div>
-                  <p class="item-desc">{{ project.endDate }}</p>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="item">Task Duration</div>
-              <p class="item-desc">{{ project.duration }} hours</p>
-              <div class="item">Task Age</div>
-              <p class="item-desc">{{ project.age }} days</p>
-              <div class="item">Task Status</div>
-              <p v-if="project.isComplete">Complete</p>
-              <p v-if="!project.isComplete">In Progress</p>
-              <div class="">
-                <button
-                  type="button"
-                  @click="deleteTask(taskParam, project.parentId)"
-                  class="btn btn-danger"
-                >
-                  Delete
-                </button>
-                <button type="button" class="btn btn-outline-primary">
-                  Update
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <UICard>
-          <!-- {{findActionsByTask(param)}} -->
-          <ActionsItems
-            v-for="action in findActionsByTask(taskParam)"
-            :key="action.id"
-            :id="action.id"
-            :type="action.type"
-            :name="action.name"
-            :category="action.category"
-          />
-        </UICard>
-      </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-if="length(param) < 1">No tasks available at this moment</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.btn {
-  margin-right: 1rem;
+.task-list {
+  max-width: 1500px;
+  margin: 0 auto;
 }
-.col {
-  background-color: rgb(255, 255, 255);
-  padding: 1rem 0;
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  /* border: 1px solid #ddd; */
 }
-.header {
+
+th,
+td {
+  text-align: left;
+
+  /* font-size: 1.2rem;*/
+}
+
+td {
+}
+
+.table-header {
   background-color: rgb(227, 239, 253);
-  /* border: solid rgb(205, 205, 205) 1px ; */
-  padding: 0.5rem 0rem;
+  /* border: solid rgb(159, 159, 159) 1px; */
 }
-.item-desc {
-  padding: 0 rem;
-  border-bottom: solid rgb(155, 155, 155, 0.3) 1px;
-  color: rgb(88, 88, 88);
-  width: 90%;
+
+.table-header th {
+  font-size: 0.8rem;
+  border: none;
+  padding: 0.8rem 1rem;
+  /* border: solid rgb(169, 169, 169) 1px; */
+}
+.table-content {
+  color: rgb(84, 84, 84);
+  /* border: solid rgb(159, 159, 159) 0.5px; */
+  text-align: center;
+}
+.table-content td {
+  font-size: 0.8rem;
+  /* padding:2rem; */
+
+  padding: 1rem 1rem;
+
+  color: rgb(84, 84, 84);
+}
+.table-content {
+  background-color: white;
+}
+.project {
+  margin: 0 auto;
+  max-width: 1650px;
+
+  padding: 2rem;
 }
 </style>
