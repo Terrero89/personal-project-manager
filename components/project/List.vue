52bb6efd0b7240c7df5ff164310a956baa0c80bb
Terrero <sergio-terrero@hotmail.com>
2022-12-02 16:06:43 -0500

    working on pagination

--- components/project/List.vue
+++ components/project/List.vue
@@ -25,9 +25,9 @@ const onPageChange = (page) => {
 };
 
 const currPage = ref(1); //shows me the current page im in
-const prevPage = ref(0);
+
 const pagesForDisplay = ref(3); //amount of pages i want the BUTTONSto display
-const itemPerPage = ref(5); // amount of items i want to display per page
+const itemPerPage = ref(5); //FIXED AMOUNT // amount of items i want to display per page
 const showDisplayButtons = ref(3); //amount of buttons i want to display for click
 const currStartingItem = ref(0);
 const currentValue = ref(0);
@@ -37,16 +37,52 @@ const lastPage = computed(() =>
 const arrLength = computed(() => [...projectStore.projects].length);
 const firstPage = computed(() => ([...projectStore.projects].length = 1));
 
+// const endPage = computed(() =>
+//   Math.min(firstPage.value + showDisplayButtons.value - 1, lastPage.value)
+// );
+
+const pages = computed(() => {
+  const range = [];
+
+  for (let i = firstPage.value; i <= lastPage.value; i++) {
+    range.push({
+      name: i,
+      isActive: i === currPage.value,
+    });
+  }
+
+  return range;
+});
+
+//findRange to display
+const findRange = (start, end) => {
+  var ans = [];
+  for (let i = start; i <= end; i++) {
+    ans.push(i);
+    
+  }
+  return ans;
+};
 const next = () => {
   let counter = itemPerPage.value; // 5
+  let initialCounter = 1;
   const preValue = showDisplayButtons.value; // 3
   console.log("preValue" + preValue);
   currPage.value++;
   console.log(currPage.value + " page number");
   // console.log(counter + " counter");
   currStartingItem.value = currStartingItem.value + itemPerPage.value; // will increase it by 5 each click
-  if (currPage.value === lastPage.value) {
+  if (currPage.value === lastPage.value - 1) {
+    initialCounter++;
+    console.log("initial " + initialCounter);
     console.log("LAST PAGE");
+    let currVal = pagesForDisplay.value + 2; // 4
+    console.log(currVal + " currVal + 1");
+    pagesForDisplay.value = currVal;
+    console.log(pagesForDisplay.value + " currVal + 1");
+    console.log(pagesForDisplay.value);
+    pagesForDisplay.value = currVal;
+    console.log(currVal + "curr");
   }
 };
 
@@ -79,6 +115,8 @@ fetchProjects();
     </UICard>
 
     <UICard>
+      <div class="">Range {{ findRange(currPage, lastPage) }}</div>
+      <div class="">{{ pages }}</div>
       <div>{{ currPage }}</div>
       <div>firstPage = {{ firstPage }}</div>
       <div>totalPages = {{ lastPage }}</div>
@@ -87,9 +125,10 @@ fetchProjects();
       <div>current Value = {{ currentValue }}-</div>
       <div>currStartingPoint = {{ currStartingItem }}</div>
       <div>showButtonCounter = {{ showDisplayButtons }}</div>
+      <div>pages for display {{ pagesForDisplay }}</div>
       <div
         class="d-flex justify-content-center"
-        v-for="i in pagesForDisplay"
+        v-for="i in findRange(currPage, lastPage)"
         :key="i"
       >
         <p>{{ i }}</p>
@@ -125,52 +164,47 @@ fetchProjects();
         </div>
       </div>
       <div class="d-flex justify-content-center">
-        <button type="button" :class="{ disabled: currPage === 1 }" @click="first">
+        <button
+          type="button"
+          :class="{ disabled: currPage === 1 }"
+          @click="first"
+        >
           |--
         </button>
-        <button type="button"  :class="{ disabled: currPage === 1 }" @click="prev">
+        <button
+          type="button"
+          :class="{ disabled: currPage === 1 }"
+          @click="prev"
+        >
           Prev
         </button>
-        <button type="button"  v-for="page in pagesForDisplay" :key="page">
-          <div type="button"  :class="{ active: page === currPage }">{{ page }}</div>
+        <button
+          type="button"
+          v-for="page in findRange(currPage, lastPage)"
+          :key="page"
+        >
+          <div type="button" :class="{ active: page === currPage }">
+            {{ page }}
+          </div>
         </button>
-        <button type="button"  :class="{ disabled: currPage === lastPage }" @click="next">
+        <button
+          type="button"
+          :class="{ disabled: currPage === lastPage }"
+          @click="next"
+        >
           Next</button
         >{{ last() }}
-        <button type="button"  :class="{ disabled: currPage === lastPage }" @click="last">
+        <button
+          type="button"
+          :class="{ disabled: currPage === lastPage }"
+          @click="last"
+        >
           --|
         </button>
       </div>
     </UICard>
 
-    <!-- <UICard>
  
-      <div class="container">
-        <div class="page-top">
-          <div class="row mb-3">
-            <div class="col"></div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="">
-            <ProjectListItem
-              v-for="project in searchedProjects"
-              :key="project.id"
-              :id="project.id"
-              :project="project.projectName"
-              :category="project.category"
-              :description="project.projectDescription"
-              :status="project.isComplete"
-            />
-          </div>
-
-          <div v-if="!projectStore.projectList.length > 0">
-            No Projects available at this moment
-          </div>
-        </div>
-      </div>
-    </UICard> -->
   </div>
 </template>
 
